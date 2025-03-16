ca396e8b814722f599f9ae99872e093d9b7bcc7a
VikumChathuranga22434 <chathx.vik@gmail.com>
2025-03-17 00:37:31 +0530

    feat: add new Home and Courses pages with styling and structure; update .gitignore for pnpm lock files

--- source/frontend/src/App.tsx
+++ source/frontend/src/App.tsx
@@ -1,41 +1,50 @@
-import {
-  Box,
-  Button,
-  Center,
-  Code,
-  Heading,
-  HStack,
-  Text,
-  VStack,
-} from '@chakra-ui/react';
-import { Tooltip } from '@/components/ui/tooltip';
-import { Logo } from '@/components/logo';
-import { ColorModeButton } from '@/components/ui/color-mode';
+// import {
+//   Box,
+//   Button,
+//   Center,
+//   Code,
+//   Heading,
+//   HStack,
+//   Text,
+//   VStack,
+// } from "@chakra-ui/react";
+// import { Tooltip } from "@/components/ui/tooltip";
+// import { Logo } from "@/components/logo";
+// import { ColorModeButton } from "@/components/ui/color-mode";
+import Courses from "./Pages/Courses";
+import Home from "./Pages/Home";
 
 function App() {
   return (
-    <Center minH="dvh">
-      <Box pos="absolute" top="4" right="4">
-        <ColorModeButton />
-      </Box>
-      <VStack gap="5">
-        <Logo height="12" mb="5" />
-        <Heading size="4xl">Welcome to Chakra UI v3</Heading>
-        <Text>Use this template to play around or report issues</Text>
-        <HStack>
-          <Tooltip
-            showArrow
-            content="This is the tooltip content"
-            contentProps={{ css: { '--tooltip-bg': 'colors.red.500' } }}
-          >
-            <Button variant="outline" size="sm">
-              Hover me
-            </Button>
-          </Tooltip>
-        </HStack>
-        <Code variant="surface">npm i @chakra-ui/react</Code>
-      </VStack>
-    </Center>
+    <>
+      <div className=''>
+        <Home />
+        <Courses />
+      </div>
+    </>
+    // <Center minH='dvh'>
+    // {/* <Box pos='absolute' top='4' right='4'>
+    //   <ColorModeButton />
+    // </Box>
+    // <VStack gap='5'>
+    //   <Logo height='12' mb='5' />
+    //   <Heading size='4xl'>Welcome to Chakra UI v3</Heading>
+    //   <Text>Use this template to play around or report issues</Text>
+    //   <HStack>
+    //     <Tooltip
+    //       showArrow
+    //       content='This is the tooltip content'
+    //       contentProps={{ css: { "--tooltip-bg": "colors.red.500" } }}
+    //     >
+    //       <Button variant='outline' size='sm'>
+    //         Hover me
+    //       </Button>
+    //     </Tooltip>
+    //   </HStack>
+    //   <Code variant='surface'>npm i @chakra-ui/react</Code>
+    // </VStack> */}
+
+    // </Center>
   );
 }
 
