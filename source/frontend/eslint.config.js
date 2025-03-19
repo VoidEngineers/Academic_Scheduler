3939522a10c4edcd299c9b2c89c5e6e3d9b38271
ravin00 <ravinbandara76@gmail.com>
2025-03-19 23:09:11 +0530

    feat: update TypeScript configuration and ESLint setup; enhance Vite and Emotion integration

--- source/frontend/eslint.config.js
+++ source/frontend/eslint.config.js
@@ -2,45 +2,27 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from '@typescript-eslint/eslint-plugin'
+import tseslint from 'typescript-eslint'
 
-export default {
-  ignores: ['dist'],
-  extends: [
-    js.configs.recommended,
-    'plugin:@typescript-eslint/recommended',  // Correctly extend the TypeScript recommended config
-    'plugin:react-hooks/recommended',
-  ],
-  files: ['**/*.{ts,tsx}'],
-  languageOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    globals: {
-      ...globals.browser,
-      es2020: true,
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
-    parser: tseslint.parser,
-    parserOptions: {
-      project: ['./tsconfig.json'],
-      tsconfigRootDir: process.cwd(),
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
     },
   },
-  plugins: {
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
-    '@typescript-eslint': tseslint,
-  },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    '@typescript-eslint/no-empty-object-type': 'warn',
-    '@typescript-eslint/no-empty-interface': 'warn',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-  env: {
-    browser: true,
-    es2020: true,
-  },
-}
+)
