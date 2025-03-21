ecc0930657bdbd6f3d567d381e0c4317147b83b8
ravin00 <ravinbandara76@gmail.com>
2025-03-21 21:09:46 +0530

    feat: implement user management module with repositories and application configuration

--- source/usermanager/src/main/java/com/Academic/Scheduler/usermanagement/UsermanagementApplication.java
+++ source/usermanager/src/main/java/com/Academic/Scheduler/usermanagement/UsermanagementApplication.java
@@ -1,4 +1,4 @@
-package com.Acedemic.Scheduler.usermanagement;
+package com.Academic.Scheduler.usermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
