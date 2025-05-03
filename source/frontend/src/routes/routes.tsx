83e57090141f0a999e7bf5a4a1e45ed55ddd7d92
Kanishka Gimhani <116288914+Kesh02@users.noreply.github.com>
2025-05-03 18:38:03 +0530

    Gimhani/feature/frontendcourse (#52)
    * Enhance Course Management UI: Update button styles,
    
    * dashboard updated
    
    * update navbar and co

--- source/frontend/src/routes/routes.tsx
+++ source/frontend/src/routes/routes.tsx
@@ -23,7 +23,7 @@ const Calendar = lazy(() => import("../pages/schdules/Calander"));
 const UserManagement = lazy(() => import("../pages/userManagement/user"));
 const ProfilePage = lazy(() => import("../pages/user/ProfilePage"));
 const AllScheduleView = lazy(() => import("../pages/schdules/AllScheduleView"));
-
+const CoursesPage = lazy(() => import("../pages/CoursePage/CourseList"));
 
 
 const EnrolledCourses = lazy(() =>
@@ -168,10 +168,10 @@ const routes: RouteWithAdminNav[] = [
     path: "/conflict-manager",
     element: <div>Conflict Manager</div>,
   },
-
-
-
-
+  {
+    path: "/allcourses",
+    element: <CoursesPage />,
+  },
 
 
 
