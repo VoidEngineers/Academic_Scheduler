a5d0dba0cb1ee0cbc7e25622a01305eb908d2366
ravin00 <ravinbandara76@gmail.com>
2025-05-15 11:15:57 +0530

    Refactor chart imports and create index files for CourseChart and LecturerChart components

---
+++ source/frontend/src/components/charts/courseChart/index.ts
@@ -0,0 +1,4 @@
+export { Pie } from 'react-chartjs-2';
+export { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+export { Card } from '../../../styles/courseChartStyles';
+export { default as CourseChart } from './courseChart';
